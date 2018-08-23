76c1603ed718540676699ed51e97f38ea3640bbf
schil227 <schil227@morris.umn.edu>
2018-08-22 21:03:45 -0500

    Added a blink endpoint, fixed an issue with checking the pin state

--- beepbeep/src/main/java/com/avionte/status/beepbeep/StatusBoardController.java
+++ beepbeep/src/main/java/com/avionte/status/beepbeep/StatusBoardController.java
@@ -6,29 +6,65 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
 @RestController
 public class StatusBoardController {
+	private static GpioPinDigitalOutput pin;
+	
+	public StatusBoardController() {
+		GpioController gpio = GpioFactory.getInstance();
+		pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Pin 1", PinState.LOW);
+	}
+	
 	@RequestMapping("status/")
 	public String index() {
-		return "You've reached the status controller";
+		return "You've reached the status controller\n";
 	}
 	
 	@RequestMapping("status/toggle")
-	public void toggle() {
-		System.out.println("toggling...");
+	public String toggle() {
+		String message = "";
 		
-		GpioController gpio = GpioFactory.getInstance();
+		PinState state = pin.getState();
 		
-		GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+		System.out.println("Pin State: " + state +" - isHigh: " + state.isHigh() + " - isLow: " + state.isLow());
 		
-		if(pin.isHigh()) {
+		if(state.isHigh()) {
+			message = "Setting PIN 1 output to low.\n";
+			System.out.println(message);
+			
 			pin.low();
 		} else {
+			message = "Setting PIN 1 output to high.\n";
+			System.out.println(message);
+			
 			pin.high();
 		}
 		
-		gpio.unprovisionPin(pin);
+		return message;
+	}
+	
+	@RequestMapping("status/blink")
+	public String blink() throws InterruptedException {
+		String message = "";
+		
+		pin.high();
+		
+		Thread.sleep(5000);
+		
+		pin.low();
+		
+		Thread.sleep(5000);
+		
+		pin.high();
+		
+		Thread.sleep(5000);
+		
+		pin.low();
+		
+		return "Blink!\n";
+//		gpio.unprovisionPin(pin);
 	}
 }
